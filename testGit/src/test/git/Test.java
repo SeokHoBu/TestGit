7c0d5ac80575ba8da6ae785be40863dad1fa254f
SeokHoBu <bu0023@naver.com>
2017-02-13 16:11:23 +0900

    테스트1

---
+++ testGit/src/test/git/Test.java
@@ -0,0 +1,13 @@
+package test.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("깃 테스트 입니다.");
+		for(int i=0;i<5;i++){
+			System.out.println("Git :"+i);
+		}
+
+	}
+
+}
