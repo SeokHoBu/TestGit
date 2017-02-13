5517c20e1b3c3e74c83c9734a13ca5119af5b473
SeokHoBu <bu0023@naver.com>
2017-02-13 16:20:41 +0900

    test2

--- testGit/src/test/git/Test.java
+++ testGit/src/test/git/Test.java
@@ -1,12 +1,15 @@
 package test.git;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("깃 테스트 입니다.");
-		for(int i=0;i<5;i++){
-			System.out.println("Git :"+i);
-		}
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("번호 입력");
+		int a = sc.nextInt();
+		
 
 	}
 
